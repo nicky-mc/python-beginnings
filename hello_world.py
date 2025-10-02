0f99c526f10ef860241e210ce265afa85b782269
Nicky Mortoza-Cowles <cinimodazotrom@gmail.com>
2025-10-02 16:02:13 +0100

    first bit of python simple print and strings and f-string

---
+++ hello_world.py
@@ -0,0 +1,32 @@
+print("hello, world!")
+# This is a comment in the code you would use to explain what the code does.
+# The code above prints "hello, world!" to the console.
+print("This line will be executed.")
+# The line below is commented out and will not be executed.
+# print("This line will not be executed."
+age = 30
+name = "Guido van Rossum"
+print(age)
+print(name)
+print("I am " + str(age) + " years old.")
+print("My name is " + name + ".")
+greeting = "Hello, Python!"
+fact = 'Python is fun.'
+print(greeting)
+print(fact)
+# The code below demonstrates the use of single and double quotes in strings.
+quote = 'He said, "Python is awesome!"'
+print(quote)
+# The code below demonstrates the use of escape characters in strings.
+escaped_quote = "He said, \"Python is awesome!\""
+name = "Bob"
+age = 37
+print(name + " is " + str(age) + " years old.")
+# The code above concatenates strings and variables to form a complete sentence.
+print(f"{name} is {age} years old.")
+# The code above uses an f-string to format the output. an f-string is a string literal that is prefixed with 'f' or 'F' and contains expressions inside curly braces {} that are evaluated at runtime.
+fruits = ["apple", "banana", "cherry"]
+mixed_list = [1, "two", 3.0, True]
+print(fruits)
+print(mixed_list)
+
